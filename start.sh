13534e23c2eaad37811f8514acb2fe1d87730b5c
candycrushpro <bnilmar54@gmail.com>
2020-09-30 14:27:11 +0300

    SQL bd

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 export SECRET_KEY='xyz123abc'
 export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/blogposts'
+export BLOG_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
 export MAIL_USERNAME=bradley
 export MAIL_PASSWORD=bradley.com
 export DATABASE_URL = 'postgres://hparpklljlfoos:417a0ed7fd21f18cc1c83bb1d99fe1ee64e9c746e5011cc3d7162848bd08e357@ec2-3-210-255-177.compute-1.amazonaws.com:5432/de3oalj9a9hj9i'
