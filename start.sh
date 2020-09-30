a58234a3f9bc70b45ff17a50ae519d81936c354b
candycrushpro <bnilmar54@gmail.com>
2020-09-30 13:42:55 +0300

    final changes

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 export SECRET_KEY='xyz123abc'
+export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/blogposts'
 export MAIL_USERNAME=bradley
 export MAIL_PASSWORD=bradley.com
 
